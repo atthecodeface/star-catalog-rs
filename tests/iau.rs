fb0b275e2ab238188350b87b57ad46f8e38d035e
Gavin Stark <atthecodeface@gmail.com>
2025-08-21 11:56:02 +0100

    Fixed IAU test, test_catalog is all hipparcos stars brighter than mag 7.0

--- tests/iau.rs
+++ tests/iau.rs
@@ -4,6 +4,14 @@ use star_catalog::{iau, Catalog, Subcube};
 
 #[test]
 fn test_iau() -> Result<(), Box<dyn Error>> {
+    const IGNORE_IDS: &[usize] = &[
+        90004, 29550, 57820, 72845, 118319, 13993, 6643, 48711, 107251, 12191, 37284, 1547, 3479,
+        54158, 66047, 114322, 5529, 2247, 84832, 61177, 42446, 21109, 55174, 80076, 47087, 15578,
+        108375, 47202, 76351, 92895, 12961, 66192, 110813, 30860, 30905, 52521, 82651, 72339,
+        117291, 22491, 40687, 48235, 5054, 32916, 74961, 31895, 13192, 99711, 88414, 116084, 83547,
+        81022, 56572, 106824, 55664, 79431, 95262, 104780, 43674, 110458, 38041, 17096, 57291,
+        96078, 87937, 80838, 70890,
+    ];
     let (is_hipparcos, s) = match std::fs::read_to_string("hipparcos.json") {
         Ok(s) => (true, s),
         Err(s) => (false, std::fs::read_to_string("test_catalog.json")?),
@@ -14,6 +22,11 @@ fn test_iau() -> Result<(), Box<dyn Error>> {
     catalog.derive_data();
     eprintln!("Loaded {} stars", catalog.len());
     for (name, opt_id, ra, de) in iau::NAMES_AND_RA_DE.iter() {
+        if let Some(id) = opt_id {
+            if IGNORE_IDS.contains(id) {
+                continue;
+            }
+        }
         let ra = (*ra as f64) / 180.0 * std::f64::consts::PI;
         let de = (*de as f64) / 180.0 * std::f64::consts::PI;
         let subcube_iter = Subcube::iter_all();
